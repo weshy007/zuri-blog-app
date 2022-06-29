8e23d97f4072d655ce1adab461c4792ea5b7e702
weshy <josephwaweru96@gmail.com>
2022-06-29 14:27:12 +0300

    serializers and views

--- links/urls.py
+++ links/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'links'
 
 urlpatterns = [ 
-
+    path("create/", views.PostCreateAPI.as_view(), name="api_create"),
+    path("update/<int:pk>", views.PostUpdateAPI.as_view(), name="api_update"),
+    path("delete/<int:pk>", views.PostDeleteAPI.as_view(), name="api_delete"),
+    path("", views.PostListAPI.as_view(), name="api_list"),
 ]
--- links/views.py
+++ links/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Link
+from .serilaizers import LinkSerializer
+
 
 # Create your views here.
+class PostListAPI(generics.ListAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostCreateAPI(generics.CreateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostDetailAPI(generics.RetrieveAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostUpdateAPI(generics.UpdateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostDeleteAPI(generics.DestroyAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
